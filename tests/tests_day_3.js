addb2ab5ec0d438f704f1aafbfa72c1324c1da6c
sneakykate <sneakyk8@gmail.com>
2016-12-06 22:00:45 -0800

    day 3 tests

--- tests/tests_day_3.js
+++ tests/tests_day_3.js
@@ -1,2 +1,15 @@
 import {expect} from 'chai';
-import {solution} from '../day-3';
+import {solution, data} from '../day-3';
+
+describe('day 3 - triangles', ()=>{
+  it('rejects 10/5/25', ()=>{
+    expect(solution('10 5 25')).to.equal(0);
+  })
+  it('is cool with 10/5/8', ()=>{
+    expect(solution('10 5 8')).to.equal(1);
+  })
+  it('gives me the answer to the puzzle', ()=>{
+    console.log(solution(data));
+    expect(solution(data)).to.be.ok;
+  })
+})
