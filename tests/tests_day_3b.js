8c70f604a3d815f5955dd8c861fc449abd5b2326
sneakykate <sneakyk8@gmail.com>
2016-12-07 17:29:10 -0800

    start tests for day 3b

---
+++ tests/tests_day_3b.js
@@ -0,0 +1,15 @@
+import {expect} from 'chai';
+import {solution, data} from '../day-3b';
+
+describe('day 3 - triangles', ()=>{
+  it('rejects 10/5/25 vertically', ()=>{
+    expect(solution('10 5 25 /n 5 25 10 /n 25 10 5')).to.equal(0);
+  })
+  it('is cool with 10/5/8', ()=>{
+    expect(solution('10 5 8')).to.equal(1);
+  })
+  it('gives me the answer to the puzzle', ()=>{
+    console.log(solution(data));
+    expect(solution(data)).to.be.ok;
+  })
+})
