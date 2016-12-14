24a550da62f3590b0d24ed7ce001c3422ac8b2b6
sneakykate <sneakyk8@gmail.com>
2016-12-13 22:18:26 -0800

    tests for this one are weird

--- tests/tests_day_4b.js
+++ tests/tests_day_4b.js
@@ -1,19 +1,12 @@
 import {expect} from 'chai';
-import {solution, data} from '../day-4';
+import {solution, data} from '../day-4b';
 
-describe('day 4 - real rooms', ()=>{
+describe('day 4b - real rooms', ()=>{
   it('rejects totally-real-room-200[decoy]', ()=>{
     expect(solution('totally-real-room-200[decoy]')).to.equal(0);
   })
-  it('accepts a-b-c-d-e-f-g-h-987[abcde] as a real room', ()=>{
-    expect(solution('a-b-c-d-e-f-g-h-987[abcde]')).to.equal(987);
-  })
-  it('accepts aaaaa-bbb-z-y-x-123[abxyz] as a real room', ()=>{
-    expect(solution('aaaaa-bbb-z-y-x-123[abxyz]')).to.equal(123);
-  })
-  it('adds solutions together', ()=>{
-    expect(solution(`aaaaa-bbb-z-y-x-123[abxyz]
-      a-b-c-d-e-f-g-h-987[abcde]`)).to.equal(1110);
+   it('gives 1 as ops-u-i-1[iopsu]', ()=>{
+    expect(solution('ops-u-ii-1[iopsu]')).to.equal(1);
   })
   it('gives me the answer to the puzzle', ()=>{
     console.log(solution(data));
