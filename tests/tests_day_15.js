048b96e597214698966c7502622fdfa8fa9d4d44
sneakykate <sneakyk8@gmail.com>
2016-12-15 18:12:56 -0800

    test day 15

---
+++ tests/tests_day_15.js
@@ -0,0 +1,10 @@
+import {expect} from 'chai';
+import {solution, data} from '../day-15';
+
+solution(data);
+
+// describe('day 15 - discs & capsule', ()=>{
+//   it('logs something with real data', ()=>{
+//     expect(solution(data)).to.equal(0);
+//   })
+// })
