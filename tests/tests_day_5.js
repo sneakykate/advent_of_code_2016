2909a4e5f9080f673198c364095034e0421b9cc9
sneakykate <sneakyk8@gmail.com>
2017-01-03 22:29:38 -0800

    set up tests day 5

---
+++ tests/tests_day_5.js
@@ -0,0 +1,15 @@
+import {expect} from 'chai';
+import {solution, data} from '../day-5';
+
+describe('day 5 - passMd5', ()=>{
+  it('rejects password', ()=>{
+    expect(solution('abc')).to.not.equal('password');
+  })
+  it('works with demo data', ()=>{
+    expect(solution('abc')).to.equal('18f47a30');
+  })
+  it('gives me the answer to the puzzle', ()=>{
+    console.log(solution(data));
+    expect(solution(data)).to.be.ok;
+  })
+})
