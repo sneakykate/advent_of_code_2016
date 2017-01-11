d2be1120c1b1884e5298f911cea06cf93d6caddd
sneakykate <sneakyk8@gmail.com>
2017-01-11 09:24:50 -0800

    crashes

--- day-5.js
+++ day-5.js
@@ -2,16 +2,19 @@ var md5 = require("blueimp-md5")
 
 function getPass(input){
   let i = 0;
-  let nextHash = md5(input + i);
-  let checkIfZeros = nextHash.slice(0,5);
   let pass = '';
-
+  let checkIfZeros;
   while(checkIfZeros !== '00000' && pass.length < 8){
+    console.log(checkIfZeros);
+    console.log('while loop runs')
+    let nextHash = md5(input + i);
+   checkIfZeros = nextHash.slice(0,5);
     i++;
     if (checkIfZeros === '00000'){
       pass += nextHash[5];
     }
   }
+  console.log(pass);
   return pass;
 }
 
