fc7b3bfc7bebe1176b92392dddcbed714d255856
sneakykate <sneakyk8@gmail.com>
2017-01-11 23:01:30 -0800

    set up day 6

--- day-5.js
+++ day-5.js
@@ -1,29 +1,29 @@
-var md5 = require("blueimp-md5")
+// var md5 = require("blueimp-md5")
 
-function getPass(input){
-  let i = 0;
-  let pass = '';
-  let checkIfZeros;
-  while(checkIfZeros !== '00000' && pass.length < 8 && i < 4000000){
-    console.log(checkIfZeros);
-    console.log('while loop runs')
-    console.log(input + i);
-    let nextHash = md5(input + i);
-   checkIfZeros = nextHash.slice(0,5);
+// function getPass(input){
+//   let i = 0;
+//   let pass = '';
+//   let checkIfZeros;
+//   while(checkIfZeros !== '00000' && pass.length < 8 && i < 4000000){
+//     console.log(checkIfZeros);
+//     console.log('while loop runs')
+//     console.log(input + i);
+//     let nextHash = md5(input + i);
+//    checkIfZeros = nextHash.slice(0,5);
    
-    i++;
-    if (checkIfZeros === '00000'){
-      pass += nextHash[5];
-    }
-    console.log(pass);
-  }
-  console.log('md5 demo', md5('abc3231929'));
+//     i++;
+//     if (checkIfZeros === '00000'){
+//       pass += nextHash[5];
+//     }
+//     console.log(pass);
+//   }
+//   console.log('md5 demo', md5('abc3231929'));
   
-  return pass;
-}
+//   return pass;
+// }
 
-const testData = `abc`;
-const realData = `reyedfim`;
+// const testData = `abc`;
+// const realData = `reyedfim`;
 
-module.exports.solution = getPass;
-module.exports.data = testData;
+// module.exports.solution = getPass;
+// module.exports.data = testData;
---
+++ tests/tests_day_6.js
@@ -0,0 +1,15 @@
+import {expect} from 'chai';
+import {solution, data, testData} from '../day-6';
+
+describe('day 6 - repetition', ()=>{
+  it('rejects password', ()=>{
+    expect(solution('abc')).to.not.equal('password');
+  })
+  it('works with demo data', ()=>{
+    expect(solution(testData)).to.equal('easter');
+  })
+  // it('gives me the answer to the puzzle', ()=>{
+  //   console.log(solution(data));
+  //   expect(solution(data)).to.be.ok;
+  // })
+})
